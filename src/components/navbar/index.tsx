6d29f1080feb96b3e64ebfc2ce16703a8df1d2de
DanielBritoSJunior <danielbritodasilvajunior@gmail.com>
2024-11-14 00:18:27 -0300

    adicionando codigo da navbar

--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -4,12 +4,24 @@ interface NavbarProps {
     active: pageIndex
 }
 
-export default function navbar({ active }: NavbarProps) {
+import Link from "next/link"
+
+export default function Navbar() {
     return (
         <>
-            <div className="flex justify-around">
-                <div className="O que é?"></div>
-            </div>
+            <body>
+                <div className="bg-white-100 h-screen flex flex-col px-0">
+                    <nav className="flex justify-center bg-green-200 p-3">
+                        <Link href="/oque" className="mx-4 px-4 py-2 font-semibold text-black-700 hover:bg-green-300 rounded-md">Oque é?</Link>
+                        <Link href="/historico" className="mx-4 px-4 py-2 font-semibold text-black-700 hover:bg-green-300 rounded-md">Histórico</Link>
+                        <Link href="/atualmente" className="mx-4 px-4 py-2 font-semibold text-black-700 hover:bg-green-300 rounded-md">Atualmente</Link>
+                        <Link href="/sintomas" className="mx-4 px-4 py-2 font-semibold text-black-700 hover:bg-green-300 rounded-md">Sintomas</Link>
+                        <Link href="/diagnostico" className="mx-4 px-4 py-2 font-semibold text-black-700 hover:bg-green-300 rounded-md">Diagnósticos</Link>
+                        <Link href="/tratamento" className="mx-4 px-4 py-2 font-semibold text-black-700 hover:bg-green-300 rounded-md">Tratamento</Link>
+                        <Link href="/prevencao" className="mx-4 px-4 py-2 font-semibold text-black-700 hover:bg-green-300 rounded-md">Prevenção</Link>
+                    </nav>
+                </div>
+            </body>
         </>
     )
 }
