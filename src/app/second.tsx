b540de212eb1e91c6201d2325f123b410b0205d0
ericmeneg <ericvinicius.menegon@gmail.com>
2024-11-14 08:21:04 -0300

    Removendo a classe h-screen do componente navbar, criando a estrutura de diretórios para as páginas

---
+++ src/app/second.tsx
@@ -0,0 +1,7 @@
+import Navbar from "@/components/teste";
+
+export default function second() {
+    return (
+        <Navbar></Navbar>
+    )
+}
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -10,15 +10,17 @@ export default function Navbar() {
     return (
         <>
             <body>
-                <div className="bg-white-100 h-screen flex flex-col px-0">
+                <div className="bg-white-100 flex flex-col px-0">
                     <nav className="flex justify-center bg-green-200 p-3">
-                        <Link href="/oque" className="mx-4 px-4 py-2 font-semibold text-black-700 hover:bg-green-300 rounded-md">Oque é?</Link>
+                        <Link href="/" className="mx-4 px-4 py-2 font-semibold text-black-700 hover:bg-green-300 rounded-md">Home</Link>
+                        <Link href="/oque" className="mx-4 px-4 py-2 font-semibold text-black-700 hover:bg-green-300 rounded-md">O que é?</Link>
                         <Link href="/historico" className="mx-4 px-4 py-2 font-semibold text-black-700 hover:bg-green-300 rounded-md">Histórico</Link>
                         <Link href="/atualmente" className="mx-4 px-4 py-2 font-semibold text-black-700 hover:bg-green-300 rounded-md">Atualmente</Link>
                         <Link href="/sintomas" className="mx-4 px-4 py-2 font-semibold text-black-700 hover:bg-green-300 rounded-md">Sintomas</Link>
                         <Link href="/diagnostico" className="mx-4 px-4 py-2 font-semibold text-black-700 hover:bg-green-300 rounded-md">Diagnósticos</Link>
                         <Link href="/tratamento" className="mx-4 px-4 py-2 font-semibold text-black-700 hover:bg-green-300 rounded-md">Tratamento</Link>
                         <Link href="/prevencao" className="mx-4 px-4 py-2 font-semibold text-black-700 hover:bg-green-300 rounded-md">Prevenção</Link>
+                        <Link href="/matematica" className="mx-4 px-4 py-2 font-semibold text-black-700 hover:bg-green-300 rounded-md">Modelos</Link>
                     </nav>
                 </div>
             </body>
